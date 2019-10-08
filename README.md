b63f8e113e6804909bf744a3e417c51108c6d489
Mrzhouqifei <18401620071@163.com>
2019-10-08 19:43:40 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# AMAE
+# What Dose Questions Ask Exactly? AMAE: Attention Model withMulti-Fusion Asking Emphasis for Duplicate Question Identification∗
+
+Our code can run on four datasets: 
+
+Duplicate Question Identification DatasetsQuora Ques-tion Pairs, CQADupStack,
+
+Natural language inference Datasets: SNLI and MultiNLI.
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-python bert_quora.py > log/quora/quora_bert4.log
-python bert_snli.py >> log/snli/snli_bert4.log
-python bert_mnli.py >> log/mnli/mnli_bert4.log
+python bert_quora.py > log/quora/quora_bert.log
+python bert_cqadup.py > log/cqadup/cqadup_bert.log
+python bert_snli.py >> log/snli/snli_bert.log
+python bert_mnli.py >> log/mnli/mnli_bert.log
